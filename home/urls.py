cad94332f085a5fd6c8bf42d43ede753d8877cbb
Bastia Lazo <bastianlazo0933o@gmail.com>
2025-05-05 14:24:40 -0400

    Se agregó la vista de productos

---
+++ home/urls.py
@@ -0,0 +1,7 @@
+# home/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('productos/', views.productos, name='productos'),
+]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,14 @@
+
 from django.shortcuts import render
 
 # Create your views here.
 def homepage(request):
     return render(request, 'home/index.html')
+
+def productos(request):
+    productos = [
+        {'nombre': 'Martillo tradicional', 'precio': 3990, 'imagen': 'images/Martillo.png'},
+        {'nombre': 'Atornillador electrico', 'precio': 4490, 'imagen': 'images/atornilladorelectrico.jpg'},
+        {'nombre': 'Pintura de interior', 'precio': 2990, 'imagen': 'images/pinturainterior.jpg'},
+    ]
+    return render(request, 'home/productos.html', {'productos': productos})
